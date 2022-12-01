d1a0cf99447abf37c4ba0b9441e259749515dcbd
avg1009 <avg1009@alu.ubu.es>
2022-12-01 19:14:06 +0100

    base de la DB

---
+++ components/dms2223backend/dms2223backend/data/db/exc/__init__.py
@@ -0,0 +1,5 @@
+""" Questions  database-related exceptions.
+"""
+
+from .preguntaexistserror import PreguntaExisteError
+from .preguntanotfounderror import PreguntaNotFoundError
---
+++ components/dms2223backend/dms2223backend/data/db/exc/preguntanotfounderror.py
@@ -0,0 +1,7 @@
+""" PreguntaNotFoundError class module.
+"""
+
+
+class PreguntaNotFoundError(Exception):
+    """ Error raised when an attempt to create an existing question occurs.
+    """
