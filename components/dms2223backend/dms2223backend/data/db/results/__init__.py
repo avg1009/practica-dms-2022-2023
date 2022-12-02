bb96eb8fe2e0f65a891aa509d9ea487679a2eda1
vpr1004 <vpr1004@alu.ubu.es>
2022-12-02 12:53:31 +0100

    solucion errores al iniciar backend

---
+++ components/dms2223backend/dms2223backend/data/db/results/__init__.py
@@ -0,0 +1,6 @@
+from .comentarioDB import Comentario
+from .preguntaDB import Pregunta
+#from .reporteDB import reporte
+from .respuestaDB import Respuesta
+from .resultbase import ResultBase
+#from .votosDB import Votos
