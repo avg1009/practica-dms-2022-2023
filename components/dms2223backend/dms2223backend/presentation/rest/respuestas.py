d47732a34801c239aa2e264d66b9b033c907ee3e
mgm1027 <mgm1027@alu.ubu.es>
2022-12-10 17:39:51 +0100

    implementacion de metodos sin dependencia de la bd

--- components/dms2223backend/dms2223backend/presentation/rest/respuestas.py
+++ components/dms2223backend/dms2223backend/presentation/rest/respuestas.py
@@ -1,33 +1,32 @@
 from typing import List, Dict
 from http import HTTPStatus
 from flask import current_app
-from dms2223backend.data.db.results import Respuesta
-from dms2223backend.data.db.resultsets import Respuestas
 from dms2223backend.service import respuestaservice
+from dms2223backend.service import preguntaservice
 
-def get_respuestas() :
+def get_respuestas(qid: int) :
     with current_app.app_context() :
-        respuestas: List[Respuesta] = respuestaservice.RespuestaService.list_respuestas()
-        #incompleto
-    return respuestas, HTTPStatus.OK.value
+        if preguntaservice.PreguntaService.exits_pregunta(qid):
+            return respuestaservice.RespuestaService.list_respuestas(qid), HTTPStatus.OK.value
+        else:
+            return ("La pregunta no existe", HTTPStatus.NOT_FOUND.value)
 
 
 def post_respuesta(descripcion: str, qid: int):
     with current_app.app_context() :
-        nuevaRespuesta : Respuesta = respuestaservice.RespuestaService.create_respuesta(descripcion, qid)
-        if respuestaservice.RespuestaService.exists_respuesta(descripcion,qid):
-            return nuevaRespuesta, HTTPStatus.CREATED.value
-        else:
+        try:
+            return respuestaservice.RespuestaService.create_respuesta(descripcion, qid), HTTPStatus.CREATED.value
+        except Exception:
             return ('No se ha creado el argumento', HTTPStatus.NOT_FOUND.value)
+
 
 def get_respuesta(descripcion : str, aid : int) :
     with current_app.app_context() :
-        if (respuestaservice.RespuestaService.exists_respuesta(descripcion, aid)) :
-            respuesta : Respuesta = respuestaservice.RespuestaService.get_respuesta(aid)
+        if (respuestaservice.RespuestaService.exists_respuesta(aid)) :
+            return respuestaservice.RespuestaService.get_respuesta(aid),HTTPStatus.OK.value
         else :
             return ('No se ha encontrado el argumento', HTTPStatus.NOT_FOUND.value)
         
-    return respuesta, HTTPStatus.OK.value
 
 def post_voto(aid: int):
     pass
